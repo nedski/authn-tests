db91d2a15c47ae6ad6e1bfdb107fa9b5ac8b25e3
Neil Kohl <neil@kohlweb.com>
2013-03-15 16:48:13 -0400

    Document subtree and stub of recipe for creating default site with appropriate access control

---
+++ local-cookbooks/authn_tests/recipes/test-site.rb
@@ -0,0 +1,9 @@
+# see http://docs.opscode.com/resource_remote_directory.html
+# copy test html documents to public folder
+# remote_directory "web-dir-name " do
+#   some_attribute "value" # see attributes section below
+#   ...
+#   action :action # see actions section below
+# end
+
+# replace default www config with custom one using appropriate authen/authz

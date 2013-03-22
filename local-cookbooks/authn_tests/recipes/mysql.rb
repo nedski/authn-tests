eaabdfcfd178785ce8b683a40fafb79ece729ff6
Neil Kohl <neil@kohlweb.com>
2013-03-22 12:12:26 -0400

    mysql test site provisioned with test pages, but authen/authz config still not finished.

--- local-cookbooks/authn_tests/recipes/mysql.rb
+++ local-cookbooks/authn_tests/recipes/mysql.rb
@@ -2,10 +2,22 @@ include_recipe "mysql::server"
 package "libapache2-mod-auth-mysql"
 apache_module "auth_mysql"  
 
+node[:server_name] = 'neilkmini.acponline.org'
+node[:docroot]     = '/var/www/auth-mysql'
+node[:application_name] = 'auth-mysql'
 db_name = 'acp_users'
 
 cookbook_file "/tmp/init_acp_users.sql" do
   action :create
+end
+
+template '/etc/apache2/sites-available/auth-mysql' do
+  source 'default.erb'
+end
+
+remote_directory "/var/www/auth-mysql" do
+  source "test-site"
+  files_owner 'www-data'
 end
 
 # create database
@@ -20,6 +32,16 @@ execute "init-acp-users-database" do
   not_if {::File.exists?('/root/.db_init_finished')}
 end
 
+apache_site 'default' do
+  enable false
+end
+
+apache_site 'auth-mysql' do
+  enable
+end
+  
 file '/root/.db_init_finished' do
   action :touch
 end
+
+
